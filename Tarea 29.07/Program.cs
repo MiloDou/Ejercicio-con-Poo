f0fc36d81a91a31b2e598b013132be2ba7ce96c5
MiloDou <158305280+MiloDou@users.noreply.github.com>
2024-07-30 19:01:06 -0600

    Creación del menú principal

--- Tarea 29.07/Program.cs
+++ Tarea 29.07/Program.cs
@@ -1,2 +1,79 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Productos
+{
+     class program
+    {
+        static void Main(String[] args)
+        {
+            try
+            {
+                int option;
+                ShowMenu();
+                Console.Write("Choose a option: ");
+                option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        {
+
+                        }
+                        break;
+                    case 2:
+                        {
+
+                        }
+                        break;
+                    case 3:
+                        {
+
+                        }
+                        break;
+                    case 4:
+                        {
+
+                        }
+                        break;
+                    case 5:
+                        { 
+                            
+                        }break;
+                    case 6:
+                        {
+                            ExitProgram();
+                        }
+                        break;
+                }
+            }
+            catch (FormatException MessageError)
+            {
+
+                Console.WriteLine("Error found: " + MessageError);
+            }
+            catch (OverflowException MessageError1)
+            {
+                Console.WriteLine("Error foudn: " + MessageError1);
+            }
+
+
+        }
+        static void ShowMenu()
+        {
+            Console.WriteLine("------------------------------");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("       PRINCIPAL MENU");
+            Console.ResetColor();
+            Console.WriteLine("------------------------------\n");
+            Console.WriteLine("1. Add product");
+            Console.WriteLine("2. Consult product");
+            Console.WriteLine("3. Sale");
+            Console.WriteLine("4. Stock");
+            Console.WriteLine("5. Show Resumé");
+            Console.WriteLine("6. Exit");
+        }
+        static void ExitProgram()
+        {
+            Console.WriteLine("\nCiao User...");
+            Console.WriteLine("ENTER TO EXIT");
+
+        }
+    } 
+}
